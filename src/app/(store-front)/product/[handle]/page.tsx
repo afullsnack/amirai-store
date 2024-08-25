58d4493c1d3e90f60c9e3fe90200924b54c5c4c6
Miracle Friday <miraclef60@gmail.com>
2024-08-25 18:56:15 +0100

    feat: Handle product page with slug from route

--- src/app/(store-front)/product/[handle]/page.tsx
+++ src/app/(store-front)/product/[handle]/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+/***
+ * Handle product display and add to cart actions
+ * Render a client page that loads a server component
+ * > with a Suspense component and fallback rendered
+ * > when fetching data
+ */
 import { Container, Main, Section } from "@/components/craft";
+import { RenderProduct } from "@/components/Product";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
 
 export default function ProductPage({
   params,
@@ -7,9 +17,29 @@ export default function ProductPage({
 }) {
   return (
     <Main>
+      {/*Handle product details*/}
+      <Section className="grid grid-cols-1 md:grid-cols-2">
+        <Suspense
+          fallback={
+            <Container className="flex items-center gap-4 w-full">
+              <Skeleton className="min-w-[250px] min-h-32 rounded-md" />
+              <div className="flex flex-col gap-3 min-w-[250px] min-h-32">
+                <Skeleton className="w-full h-10 rounded-sm" />
+                <Skeleton className="w-full h-10 rounded-sm" />
+                <Skeleton className="w-full h-10 rounded-sm" />
+                <Skeleton className="w-full h-10 rounded-sm" />
+              </div>
+            </Container>
+          }
+        >
+          <RenderProduct slug={params?.handle} />
+        </Suspense>
+      </Section>
+
+      {/* Related products section*/}
       <Section>
         <Container>
-          <h1>{params?.handle} single product</h1>
+          <h1>Handle related products</h1>
         </Container>
       </Section>
     </Main>
