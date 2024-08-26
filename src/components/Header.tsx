b9d30d85e7751f5a180eb54dbaa94849d26bc988
Miracle Friday <miraclef60@gmail.com>
2024-08-26 19:55:57 +0100

    fix: Hide cart sheet button on checkout page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,11 +30,12 @@ import {
 import Image from "next/image";
 import { useCart } from "./cart/cart-context";
 import { Badge } from "./ui/badge";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Header() {
   const { cart, updateCartItem } = useCart();
   const { push } = useRouter();
+  const pathname = usePathname();
 
   return (
     <header className="sticky top-0 z-50 flex h-16 items-center !justify-between gap-4 border-b bg-background px-4 md:px-6">
@@ -132,89 +133,91 @@ export default function Header() {
             <DropdownMenuItem>Logout</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" className="shrink-0 gap-2 relative">
-              <ShoppingCart className="h-5 w-5" />
-              <span>${cart.totalAmount}</span>
-              <Badge className="absolute -right-2 -top-2 w-4 h-5 items-center justify-center rounded-full">
-                {cart.totalQuantity}
-              </Badge>
-              <span className="sr-only">Toggle cart</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="right">
-            <SheetHeader>
-              <SheetTitle>Shopping cart</SheetTitle>
-            </SheetHeader>
+        {!pathname.includes("checkout") && (
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" className="shrink-0 gap-2 relative">
+                <ShoppingCart className="h-5 w-5" />
+                <span>${cart.totalAmount}</span>
+                <Badge className="absolute -right-2 -top-2 w-4 h-5 items-center justify-center rounded-full">
+                  {cart.totalQuantity}
+                </Badge>
+                <span className="sr-only">Toggle cart</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="right">
+              <SheetHeader>
+                <SheetTitle>Shopping cart</SheetTitle>
+              </SheetHeader>
 
-            <div className="h-full relative overflow-y-auto">
-              <div>
-                {cart.items.map((item, _) => (
-                  <div
-                    key={item.productId}
-                    className="flex items-center gap-4 my-3"
-                  >
-                    <Image
-                      src={item.featuredImage.url}
-                      alt={item.featuredImage.altText}
-                      width={50}
-                      height={50}
-                      className="object-cover h-8 w-8 overflow-clip"
-                    />
-                    <div className="grid gap-1">
-                      <p className="text-sm font-medium leading-none">
-                        {item.name}
-                      </p>
-                      <p className="text-sm text-muted-foreground">
-                        Qty:{item.quantity} Size:{item.selectedSize}
-                      </p>
-                    </div>
-                    <div className="ml-auto font-medium flex items-center gap-4">
-                      <div className="flex items-center gap-3">
-                        <Button
-                          size="icon"
-                          variant="outline"
-                          className="h-6 w-6"
-                          onClick={() => {
-                            item.quantity === 1
-                              ? updateCartItem(item.productId, "delete")
-                              : updateCartItem(item.productId, "minus");
-                          }}
-                        >
-                          <Minus className="h-3.5 w-3.5" />
-                        </Button>
-                        <Button
-                          size="icon"
-                          variant="outline"
-                          className="h-6 w-6"
-                          onClick={() => {
-                            updateCartItem(item.productId, "plus");
-                          }}
-                        >
-                          <Plus className="h-3.5 w-3.5" />
-                        </Button>
+              <div className="h-full relative overflow-y-auto">
+                <div>
+                  {cart.items.map((item, _) => (
+                    <div
+                      key={item.productId}
+                      className="flex items-center gap-4 my-3"
+                    >
+                      <Image
+                        src={item.featuredImage.url}
+                        alt={item.featuredImage.altText}
+                        width={50}
+                        height={50}
+                        className="object-cover h-8 w-8 overflow-clip"
+                      />
+                      <div className="grid gap-1">
+                        <p className="text-sm font-medium leading-none">
+                          {item.name}
+                        </p>
+                        <p className="text-sm text-muted-foreground">
+                          Qty:{item.quantity} Size:{item.selectedSize}
+                        </p>
                       </div>
-                      ${item.price}
+                      <div className="ml-auto font-medium flex items-center gap-4">
+                        <div className="flex items-center gap-3">
+                          <Button
+                            size="icon"
+                            variant="outline"
+                            className="h-6 w-6"
+                            onClick={() => {
+                              item.quantity === 1
+                                ? updateCartItem(item.productId, "delete")
+                                : updateCartItem(item.productId, "minus");
+                            }}
+                          >
+                            <Minus className="h-3.5 w-3.5" />
+                          </Button>
+                          <Button
+                            size="icon"
+                            variant="outline"
+                            className="h-6 w-6"
+                            onClick={() => {
+                              updateCartItem(item.productId, "plus");
+                            }}
+                          >
+                            <Plus className="h-3.5 w-3.5" />
+                          </Button>
+                        </div>
+                        ${item.price}
+                      </div>
                     </div>
-                  </div>
-                ))}
-              </div>
-              <div className="absolute left-0 right-0 bottom-10 flex flex-col gap-3">
-                <div className="flex items-center justify-between px-2 py-4 border-b border-t">
-                  <span>SUB-TOTAL</span>
-                  <span>${cart.totalAmount}</span>
+                  ))}
                 </div>
-                <Button
-                  onClick={() => push(`/checkout/${crypto.randomUUID()}`)}
-                  size={"lg"}
-                >
-                  Proceed to checkout
-                </Button>
+                <div className="absolute left-0 right-0 bottom-10 flex flex-col gap-3">
+                  <div className="flex items-center justify-between px-2 py-4 border-b border-t">
+                    <span>SUB-TOTAL</span>
+                    <span>${cart.totalAmount}</span>
+                  </div>
+                  <Button
+                    onClick={() => push(`/checkout/${crypto.randomUUID()}`)}
+                    size={"lg"}
+                  >
+                    Proceed to checkout
+                  </Button>
+                </div>
               </div>
-            </div>
-          </SheetContent>
-        </Sheet>
+            </SheetContent>
+          </Sheet>
+        )}
       </div>
     </header>
   );
