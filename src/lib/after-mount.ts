546b85a1dad79ab517a9b546760f5555f76972ac
Miracle Friday <miraclef60@gmail.com>
2024-08-28 13:02:53 +0100

    refactor: Refactor after-mount hook

--- src/lib/after-mount.ts
+++ src/lib/after-mount.ts
@@ -3,17 +3,17 @@
  * params: fn - function to run and deps - dependency array for wrapped useEffect
  */
 
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from "react";
 
 export default function useEffectAfterMount(fn: () => void, deps: any[] = []) {
-  const isMounted = useRef(false)
+  const isMounted = useRef(false);
 
   useEffect(() => {
     if (!isMounted.current) {
-      isMounted.current = true
-      return
+      isMounted.current = true;
+      return;
     }
 
-    fn()
-  }, deps)
+    fn();
+  }, [...deps, fn]);
 }
