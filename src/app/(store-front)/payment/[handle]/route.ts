b909c5b87db7bbc07bb13b2270b886e7de2324bc
Miracle Friday <miraclef60@gmail.com>
2024-08-29 03:45:22 +0100

    feat:  Add payment webhook route handler

--- src/app/(store-front)/payment/[handle]/route.ts
+++ src/app/(store-front)/payment/[handle]/route.ts
@@ -1,3 +1,5 @@
+import { client } from "@/sanity/lib/client";
+import { sendInvoiceTask } from "@/trigger/invoice";
 import { NextRequest } from "next/server";
 
 export const dynamic = "force-static";
@@ -6,14 +8,34 @@ export async function POST(
   request: NextRequest,
   { params }: { params: { handle: string } },
 ) {
-  const req = await request.json();
+  const body = await request.json();
 
   console.log(
-    req,
+    body,
     ":::body from webhook call for paystack",
     params.handle,
     ":::slug for route",
   );
+
+  // TODO: get checkoutId
+  const order = await client.fetch(
+    `*[_type == "order" && orderId match "${body?.reference}"]`,
+  );
+
+  if (order) {
+    // TODO: reconstruct cart
+    const cart = JSON.parse(order?.cart);
+
+    // TODO: call trigger to send mail to customer with invoice
+    // and details to merchant
+    const handle = await sendInvoiceTask.trigger({
+      cart,
+      contact: order?.contact,
+      shipping: order?.shipping,
+    });
+
+    console.log(handle, ":::trigger handle");
+  }
 
   const data = {
     success: true,
